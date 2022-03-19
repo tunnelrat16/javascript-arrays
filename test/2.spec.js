32adbd0aa21ecf522b7b835c1004b9a27ba9c65e
Kyle Coberly <kyle.coberly@gmail.com>
2022-03-19 12:21:38 -0600

    Fix typos

--- test/2.spec.js
+++ test/2.spec.js
@@ -1,8 +1,8 @@
 const getAlphaNumericAlphabet = require("../2")
 
 describe("Exercise 2", () => {
-  it("transforms the alphabet into an array of vowels", () => {
-    const alphaNumericAlphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "&"]
+  it("returns a full alphanumeric alphabet", () => {
+    const alphaNumericAlphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "&"]
     expect(getAlphaNumericAlphabet()).toEqual(alphaNumericAlphabet)
   })
 })
--- test/3.spec.js
+++ test/3.spec.js
@@ -1,7 +1,7 @@
 const getAlphaNumericAlphabet = require("../2")
 
 describe("Exercise 3", () => {
-  it("transforms the alphabet into an array of vowels", () => {
+  it("returns the Spanish alphabet", () => {
     const spanishAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     expect(getSpanishAlphabet()).toEqual(spanishAlphabet)
   })

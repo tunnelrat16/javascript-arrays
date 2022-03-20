ba60e96e9beba9d0de2382c29a09adc759e3a86b
Kyle Coberly <kyle.coberly@gmail.com>
2022-03-19 19:35:10 -0600

    Update test

--- test/3.spec.js
+++ test/3.spec.js
@@ -2,7 +2,7 @@ const getSpanishAlphabet = require("../3")
 
 describe("Exercise 3", () => {
   it("returns the Spanish alphabet", () => {
-    const spanishAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    const spanishAlphabet = ["a", "b", "c", "ch", "d", "e", "f", "g", "h", "i", "j", "k", "l", "ll", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     expect(getSpanishAlphabet()).toEqual(spanishAlphabet)
   })
 })

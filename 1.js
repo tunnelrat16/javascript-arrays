38b97930edb768647f99a4917ceadb5ef50a0671
tunnelrat16 <tjandmatt@hotmail.com>
2022-03-21 14:06:54 -0700

    initial commit

--- 1.js
+++ 1.js
@@ -12,13 +12,13 @@ Note:
 
 */
 
-function getVowels(){
-  const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  let vowels
+function getVowels() {
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let vowels = []
 
-  // Your code here
+    vowels.push(alphabet[0], alphabet[4], alphabet[8], alphabet[14], alphabet[20], alphabet[24]); // Your code here
 
-  return vowels
+    return vowels
 }
 
 module.exports = getVowels

b6fa482c03f459e07b68730af65f57c26f81454f
tunnelrat16 <tjandmatt@hotmail.com>
2022-03-22 08:52:44 -0700

    update 3.js

--- 3.js
+++ 3.js
@@ -9,13 +9,17 @@ Given an array containing the alphanumeric alphabet, write code that returns an 
 
 */
 
-function getSpanishAlphabet(){
-  const alphaNumericAlphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "&"]
-  let spanishAlphabet
+function getSpanishAlphabet() {
+    const alphaNumericAlphabet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "&"]
 
-  // Your code here
+    let spanishAlphabet = alphaNumericAlphabet.slice(10, 36) // Your code here
 
-  return spanishAlphabet
+    spanishAlphabet.splice(14, 0, "ñ")
+    spanishAlphabet.splice(12, 0, "ll")
+    spanishAlphabet.splice(3, 0, "ch")
+
+    return spanishAlphabet
+
 }
 
 module.exports = getSpanishAlphabet
